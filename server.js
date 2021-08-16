a5592145b488913fc6f35cb1a30352f447f76ff6
Youssef Ramy <yousseflife0@gmail.com>
2021-08-16 16:10:03 +0200

    knex

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 const db = knex({
 	client: 'pg',
 	connection:{
-		host: '127.0.0.1',
-		user: 'postgres',
-		password: 'test',
-		database: 'smart-brain'
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
